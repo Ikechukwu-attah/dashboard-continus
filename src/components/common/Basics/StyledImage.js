9e9528b5967190b2a55095b5c2789f09969524e5
sirkudos <attahikechukwu@gmail.com>
2022-03-14 15:20:55 +0100

    ft-navbar completed

--- src/components/common/Basics/StyledImage.js
+++ src/components/common/Basics/StyledImage.js
@@ -1,4 +1,9 @@
 import styled from 'styled-components'
 
 
-export const StyledImage = styled.img ``
+export const StyledImage = styled.img `
+width: ${({width})=>width};
+height: ${({height})=>height};
+border-radius: ${({borderRadius})=>borderRadius};
+
+`
